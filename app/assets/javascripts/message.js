633cee6537676409f0f97996991173a03115fb83
masayan-06 <mdmkgift@gmail.com>
2019-12-09 22:50:06 +0900

    implementation_of_incremental_search_and_auto_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,47 +1,58 @@
-	
 $(function(){
-  function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
-    //メッセージに画像が含まれる場合のHTMLを作る
-    if (message.image) {
-      var html =`<div class="message">
-                  <div class="message__upper-info">
-                    <div class="message__upper-info__talker">
-                      ${message.user_name}
-                    </div>
-                    <div class="message__upper-info__date">
-                      ${message.date}
-                    </div>
-                  </div>
-                  <div class="message__text">
-                    <p class="lower-message__content">
-                      ${message.text}
-                    </p>
-                  </div>
-                </div>`
-    }
-    //メッセージに画像が含まれない場合のHTMLを作る
-    else {
-      var html =`<div class="message">
-                  <div class="message__upper-info">
-                    <div class="message__upper-info__talker">
-                      ${message.user_name}
-                    </div>
-                    <div class="message__upper-info__date">
-                      ${message.date}
-                    </div>
-                  </div>
-                  <div class="message__text">
-                    <p class="lower-message__content">
-                      ${message.text}
-                    </p>
-                    <img class="lower-message__image">
-                      ${message.image}
-                  </div>
-                </div>`
-    }
-    return html
-  }
+  var buildHTML = function(message) {
+    if (message.text && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<p class="lower-message__content">` +
+            message.text +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.text) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<p class="lower-message__content">` +
+            message.text +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message_id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
 
   $("#new_message").on('submit', function(e){
     e.preventDefault()
@@ -66,4 +77,34 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    // console.log(last_message_id)
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,7 +1,8 @@
 
   # json.id message.id
   json.text @message.text
-  json.image @message.image
-  json.created_at @message.created_at
+  json.image @message.image.url
+  json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
   json.user_name @message.user.name
-
+  #idもデータとして渡す
+  json.id @message.id
