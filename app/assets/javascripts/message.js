decfb0b7e60ab17909aaccf828727f2dec07d0da
masayan-06 <mdmkgift@gmail.com>
2019-12-14 19:34:55 +0900

    ajax_modification

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -82,34 +82,36 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    // last_message_id = $('.message:last').data("message-id");
-    var last_message_id = $('.message').last().data("message_id");
-    console.log(last_message_id);
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      // last_message_id = $('.message:last').data("message-id");
+      var last_message_id = $('.message').last().data("message_id");
 
-    $.ajax({
-      //ルーティングで設定した通りのURLを指定
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      //追加するHTMLの入れ物を作る
-      var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+        // console.log(this);
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      })
+      .fail(function() {
+        alert('エラー');
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    })
-    .fail(function() {
-      alert('error');
-    });
+    }
   };
   setInterval(reloadMessages, 7000);
 });
